b3696b6e24b01e5445371cb448059fbd1810530c
lukas-laas <lukas.s.laas@gmail.com>
2024-06-28 15:44:30 +0200

    make app async

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
-const res = await fetch("https://api.discogs.com/releases/249504");
-const data = await res.json();
 
-console.table(data);
-
-function App() {
+async function App() {
+  const res = await fetch("https://api.discogs.com/releases/249504");
+  const data = await res.json();
   return (
     <>
       <h1>{data.artists_sort}</h1>
