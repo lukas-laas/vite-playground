5ce0ccc337385943736c3f70e07980279461f459
lukas-laas <lukas.s.laas@gmail.com>
2024-06-28 15:48:44 +0200

    remove async from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
+const res = await fetch("https://api.discogs.com/releases/249504");
+const data = await res.json();
 
-async function App() {
-  const res = await fetch("https://api.discogs.com/releases/249504");
-  const data = await res.json();
+function App() {
   return (
     <>
       <h1>{data.artists_sort}</h1>
